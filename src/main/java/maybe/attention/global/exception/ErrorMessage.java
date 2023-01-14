5dc602999f6c01b9076c4f59a30ef7ca2bc6cb8c
KimHuiMang <piyrw9754@gmail.com>
2023-01-14 22:13:53 +0900

    :sparkles: 예외 핸들러 전부 등록...
    예외 핸들러 등록

--- src/main/java/maybe/attention/global/exception/ErrorMessage.java
+++ src/main/java/maybe/attention/global/exception/ErrorMessage.java
@@ -1,4 +1,11 @@
 package maybe.attention.global.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class ErrorMessage {
+    private final String message;
+    private final int status;
 }
--- src/main/java/maybe/attention/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/maybe/attention/global/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,17 @@
 package maybe.attention.global.exception.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import maybe.attention.domain.member.exception.ExistsEmailException;
+import maybe.attention.domain.member.exception.MemberNotFoundException;
+import maybe.attention.domain.member.exception.PasswordMismatchException;
+import maybe.attention.domain.schedule.exception.NotCoincideOrganizerException;
+import maybe.attention.domain.schedule.exception.NotFoundContestScheduleException;
+import maybe.attention.global.exception.ErrorMessage;
+import maybe.attention.global.security.exception.TokenExpirationException;
+import maybe.attention.global.security.exception.TokenNotValidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +19,55 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleMemberNotFoundException(HttpServletRequest request , MemberNotFoundException e) {
+        printError(request, e, e.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(ExistsEmailException.class)
+    public ResponseEntity<ErrorMessage> handleExistsEmailException(HttpServletRequest request , ExistsEmailException e) {
+        printError(request, e, e.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(TokenExpirationException.class)
+    public ResponseEntity<ErrorMessage> handleTokenExpirationException(HttpServletRequest request , TokenExpirationException e) {
+        printError(request, e, e.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(TokenNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleTokenNotValidException(HttpServletRequest request , TokenNotValidException e) {
+        printError(request, e, e.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(PasswordMismatchException.class)
+    public ResponseEntity<ErrorMessage> handlePasswordMismatchException(HttpServletRequest request , PasswordMismatchException e) {
+        printError(request, e, e.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(NotCoincideOrganizerException.class)
+    public ResponseEntity<ErrorMessage> handleNotCoincideOrganizerException(HttpServletRequest request , NotCoincideOrganizerException e) {
+        printError(request, e, e.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(NotFoundContestScheduleException.class)
+    public ResponseEntity<ErrorMessage> handleNotFoundContestScheduleException(HttpServletRequest request , NotFoundContestScheduleException e) {
+        printError(request, e, e.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
 
     private void printError(HttpServletRequest request, RuntimeException ex, String message) {
         log.error(request.getRequestURI());
