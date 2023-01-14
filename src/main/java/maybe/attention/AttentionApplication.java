c2431f2301f51ec774f81ba2394f9f3303624cfb
KimHuiMang <piyrw9754@gmail.com>
2023-01-14 17:01:07 +0900

    project setting

---
+++ src/main/java/maybe/attention/AttentionApplication.java
@@ -0,0 +1,13 @@
+package maybe.attention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttentionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttentionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maybe/attention/AttentionApplicationTests.java
@@ -0,0 +1,13 @@
+package maybe.attention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttentionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
